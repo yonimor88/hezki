03215125ea8b442b4ff8485d3500aaacb6b0f9ec
yonimor88 <yonimor88@gmail.com>
2022-11-23 10:43:58 +0200

    working on setting up the ui and api

---
+++ server/src/models/Deck.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+ const Schema = mongoose.Schema;
+//  const ObjectID = mongoose.Types.ObjectId;
+
+
+
+const DeckSchema = new Schema({
+    title: String
+  });
+
+  const DeckModel = mongoose.model("Deck", DeckSchema);
+
+
+  export default DeckModel
